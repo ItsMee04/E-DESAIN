d14fdd9d575ebb6bc82732f244ccc2f182dd6e5e
ItsMee04 <meemeeits@gmail.com>
2026-09-15 13:29:22 +0700

    Add: Penambahan Master User Permission

---
+++ app/Models/Master/UserPermission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Master;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPermission extends Model
+{
+    use HasFactory;
+
+    protected $table = 'permission';
+    protected $fillable = [
+        'user_id',
+        'module_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
+}
